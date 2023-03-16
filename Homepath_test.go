ac06e3aa6976c7f505130b6db8014822d063a1b9
Milan Lesichkov <lesichkovm@gmail.com>
2023-03-16 20:26:27 +0000

    Update Homepath_test.go

--- Homepath_test.go
+++ Homepath_test.go
@@ -8,8 +8,13 @@ import (
 func TestHomepath(t *testing.T) {
 	path := Homepath()
 
-	expected := "/home/codespace"
-	if strings.Contains(path, expected) == false {
-		t.Error("Does not contain ", expected, ", Output:", path)
+	expecteds := []string{
+		"/home/codespace",
+		"/home/runner",
+	}
+	for _, expected := range expecteds {
+		if strings.Contains(path, expected) == false {
+			t.Error("Does not contain ", expected, ", Output:", path)
+		}
 	}
 }
